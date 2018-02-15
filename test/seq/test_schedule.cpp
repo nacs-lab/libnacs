076b820004b63f328676676d8f75181b321405ba
Yichao Yu <yyc1992@gmail.com>
2018-02-14 23:05:50 -0500

    [seq] remove test for old code

--- test/seq/test_schedule.cpp
+++ test/seq/test_schedule.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
- *   Copyright (c) 2016 - 2016 Yichao Yu <yyc1992@gmail.com>             *
+ *   Copyright (c) 2018 - 2018 Yichao Yu <yyc1992@gmail.com>             *
  *                                                                       *
  *   This library is free software; you can redistribute it and/or       *
  *   modify it under the terms of the GNU Lesser General Public          *
@@ -20,136 +20,44 @@
 #  undef NDEBUG
 #endif
 
-#include <nacs-seq/seq.h>
-#include <nacs-seq/pulser.h>
 #include <nacs-utils/timer.h>
-#include <nacs-utils/log.h>
 #include <nacs-utils/base64.h>
+#include <nacs-seq/bytecode.h>
 
-#include <math.h>
-#include <inttypes.h>
 #include <iostream>
 #include <fstream>
-#include <ostream>
-#include <iomanip>
+#include <assert.h>
 
 using namespace NaCs;
 
-static const auto seq_cb = [] (auto &, uint64_t cur_t, Seq::Event evt) {
-    if (evt == Seq::Event::start) {
-        nacsLog("Start time: %" PRIu64 "\n", cur_t);
-    } else {
-        nacsLog("End time: %" PRIu64 "\n", cur_t);
-    }
-    return cur_t;
-};
-
-int main(int argn, char **argv)
+int main(int argc, char **argv)
 {
-    assert(argn == 2);
-    uint64_t tlen = 10000000;
-    // uint64_t tlen = 10000;
-    Seq::PulsesBuilder builder =
-        [] (Seq::Channel chn, Seq::Val val, uint64_t t, uint64_t tlim) -> uint64_t {
-        (void)chn;
-        (void)val;
-        (void)t;
-        (void)tlim;
-        // nacsLog("t = %" PRIu64 ", chn = (%d, %d), v = %f\n",
-        //         t, int(chn.typ), chn.id, val.val.f64);
-        return 1;
-    };
-    std::vector<Seq::Pulse> seq;
-    seq.push_back(Seq::Pulse{0, tlen, {Seq::Channel::DAC, 0},
-                [] (auto t, auto start) {
-                    return start + t;
-                }});
-    seq.push_back(Seq::Pulse{0, tlen, {Seq::Channel::DAC, 1},
-                [] (auto t, auto start) {
-                    return start - t;
-                }});
-    seq.push_back(Seq::Pulse{0, tlen, {Seq::Channel::DAC, 3},
-                [] (auto t, auto start) {
-                    return start + sin(t / 1000.0);
-                }});
-    seq.push_back(Seq::Pulse{5000, 0, {Seq::Channel::DAC, 2},
-                Seq::Val::get<double>(20)});
-    seq.push_back(Seq::Pulse{5002, 0, {Seq::Channel::DAC, 2},
-                Seq::Val::get<double>(10)});
-    seq.push_back(Seq::Pulse{5202, 0, {Seq::Channel::DAC, 2},
-                Seq::Val::get<double>(10)});
-    std::map<Seq::Channel,Seq::Val> defaults;
-    builder.schedule(Seq::Sequence(std::move(seq), std::move(defaults)), seq_cb);
+    assert(argc >= 2);
 
-    std::ofstream ostm;
-    Seq::PulsesBuilder file_builder =
-        [&] (Seq::Channel chn, Seq::Val val, uint64_t t, uint64_t tlim) -> uint64_t {
-        uint64_t min_time = 50;
-        const char *channel_name;
-        switch (chn.typ) {
-        case Seq::Channel::TTL:
-            assert(chn.id == 0);
-            channel_name = "TTL";
-            min_time = 3;
-            break;
-        case Seq::Channel::DDS_FREQ:
-            channel_name = "FREQ";
-            break;
-        case Seq::Channel::DDS_AMP:
-            channel_name = "AMP";
-            break;
-        case Seq::Channel::DAC:
-            channel_name = "DAC";
-            break;
-        case Seq::Channel::CLOCK:
-            channel_name = "CLOCK";
-            break;
-        default:
-            assert(0 && "Unknown channel type.");
-        }
-        if (!ostm.is_open())
-            return min_time;
-        if (min_time + t > tlim)
-            return 0;
-        ostm << t << ", " << channel_name;
-        if (chn.typ == Seq::Channel::TTL) {
-            ostm << "(all) = 0x" << std::hex << val.val.i32 << std::dec
-                 << std::endl;
-        } else {
-            ostm << "(" << chn.id << ") = "
-            << std::setprecision(18) << val.val.f64 << std::endl;
-        }
-        return min_time;
-    };
+    std::ifstream istm(argv[1]);
+    std::string data(std::istreambuf_iterator<char>(istm), {});
+    assert(Base64::validate((const uint8_t*)data.data(), data.size()));
+    tic();
+    auto code = Seq::Sequence::fromBase64((const uint8_t*)data.data(),
+                                          data.size()).toByteCode(nullptr);
+    printToc();
+    size_t code_len;
+    auto code2 = Seq::Sequence::fromBase64((const uint8_t*)data.data(),
+                                           data.size()).toByteCode(&code_len, nullptr);
 
-    {
-        nacsLog("NaSingleAtom\n");
-        std::ifstream istm(std::string(argv[1]) + "/na_single_atom.seq");
-        ostm.open("na_single_atom.txt");
-        std::string data(std::istreambuf_iterator<char>(istm), {});
-        assert(Base64::validate((const uint8_t*)data.data(), data.size()));
-        tic();
-        auto seq = Seq::Sequence::fromBase64((const uint8_t*)data.data(), data.size());
-        printToc();
-        tic();
-        file_builder.schedule(seq, seq_cb);
-        printToc();
-        ostm.close();
+    if (argc >= 3) {
+        std::ofstream ostm(argv[2]);
+        ostm.write((const char*)&code[0], code.size());
     }
 
-    {
-        nacsLog("CsSingleAtom\n");
-        std::ifstream istm(std::string(argv[1]) + "/cs_single_atom.seq");
-        ostm.open("cs_single_atom.txt");
-        std::string data(std::istreambuf_iterator<char>(istm), {});
-        assert(Base64::validate((const uint8_t*)data.data(), data.size()));
-        tic();
-        auto seq = Seq::Sequence::fromBase64((const uint8_t*)data.data(), data.size());
-        printToc();
-        tic();
-        file_builder.schedule(seq, seq_cb);
-        printToc();
-        ostm.close();
-    }
+    assert(code_len == code.size());
+    assert(memcmp(code2, &code[0], code_len) == 0);
+    free(code2);
+
+    // Seq::ByteCode::print(std::cout, code);
+
+    std::cout << Seq::ByteCode::count(code) << std::endl;
+    std::cout << code.size() << std::endl;
+
     return 0;
 }
