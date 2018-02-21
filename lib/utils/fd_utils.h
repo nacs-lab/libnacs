ee964886785ec8162d9abfe97aef7ceb5383272e
Yichao Yu <yyc1992@gmail.com>
2018-02-21 15:02:22 -0500

    [utils] more eventfd helpers

--- lib/utils/fd_utils.h
+++ lib/utils/fd_utils.h
@@ -25,6 +25,7 @@
 #include <sys/stat.h>
 #include <sys/file.h>
 #include <fcntl.h>
+#include <assert.h>
 #ifdef NACS_OS_LINUX
 #  include <sys/eventfd.h>
 #endif
@@ -80,9 +81,45 @@ static inline T checkErrno(T res, Arg&&... arg)
 }
 
 #ifdef NACS_OS_LINUX
+// Helper functions for event fd.
+
+// Open a eventfd and check for error
 static inline int openEvent(int init, int flags)
 {
     return checkErrno(eventfd(init, flags), "Cannot open eventfd.");
+}
+
+// Write certain number of events (default to 1) to the event fd.
+// Retry if aborted by signal and return if the write succeeded.
+static inline bool writeEvent(int fd, uint64_t numevt=1)
+{
+    while (true) {
+        auto res = write(fd, &numevt, sizeof(uint64_t));
+        if (res == -1) {
+            if (errno == EINTR)
+                continue;
+            return false;
+        }
+        assert(res == 8);
+        return true;
+    }
+}
+
+// Read events from the event fd.
+// Retry if aborted by signal and return the number of events read.
+static inline uint64_t readEvent(int fd)
+{
+    while (true) {
+        uint64_t numevt;
+        auto res = read(fd, &numevt, sizeof(uint64_t));
+        if (res == -1) {
+            if (errno == EINTR)
+                continue;
+            return 0;
+        }
+        assert(res == 8);
+        return numevt;
+    }
 }
 #endif
 
