8a74435a129fabf215fff16efd85a93177659cec
Yichao Yu <yyc1992@gmail.com>
2019-03-02 08:30:01 -0500

    [utils/dl] look for library in mingw folder on windows

--- lib/utils/dlload.cpp
+++ lib/utils/dlload.cpp
@@ -27,21 +27,98 @@
 #  include <dlfcn.h>
 #endif
 
+#include <string>
+
 namespace NaCs {
 
 namespace DL {
 
+#if NACS_OS_WINDOWS
+static HMODULE get_libnacs_handle()
+{
+    static HMODULE hdl =
+        [] {
+            HMODULE hdl;
+            GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+                               GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+                               (LPCWSTR)(uintptr_t)(&open),
+                               (HMODULE*)&hdl);
+            return hdl;
+        }();
+    return hdl;
+}
+
+static inline bool is_dir_separator(wchar_t c)
+{
+    return c == L'/' || c == L'\\';
+}
+
+static const std::wstring &get_libnacs_dir()
+{
+    static const std::wstring dir =
+        [] {
+            auto hdl = get_libnacs_handle();
+            DWORD len = GetModuleFileNameW(hdl, nullptr, 0);
+            if (!len)
+                return std::wstring();
+            std::wstring res(len, 0);
+            GetModuleFileNameW(hdl, &res[0], len);
+            while (len-- > 0) {
+                if (is_dir_separator(res[len])) {
+                    res.resize(len);
+                    return res;
+                }
+            }
+            // Shouldn't happen...
+            res = L"C:\\";
+            return res;
+        }();
+    return dir;
+}
+
+// This implementation is copied from dotnet/coreclr (MIT License)
+static bool is_full_path(wchar_t *path, DWORD len)
+{
+    // There is no way to specify a fixed path with one character (or less).
+    if (len < 2)
+        return false;
+    // There is no valid way to specify a relative path with two initial slashes or
+    // "\?" as "?" isn't valid for drive relative paths and "\??\" is equivalent to "\\?\"
+    if (is_dir_separator(path[0]))
+        return path[1] == L'?' || is_dir_separator(path[1]);
+    return len >= 3 && (path[1] == L':') && is_dir_separator(path[2]);
+}
+
 NACS_EXPORT() void *open(const char *filename, Flags flags)
 {
-#if NACS_OS_WINDOWS
-    size_t len = MultiByteToWideChar(CP_UTF8, 0, filename, -1, nullptr, 0);
+    DWORD len = MultiByteToWideChar(CP_UTF8, 0, filename, -1, nullptr, 0);
     if (!len)
         return nullptr;
     WCHAR *wfilename = (WCHAR*)alloca(len * sizeof(WCHAR));
     if (!MultiByteToWideChar(CP_UTF8, 0, filename, -1, wfilename, len))
         return nullptr;
-    return LoadLibraryExW(wfilename, nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+    if (is_full_path(wfilename, len - 1))
+        return LoadLibraryExW(wfilename, nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+    if (auto hdl = LoadLibraryExW(wfilename, nullptr, 0))
+        return hdl;
+    auto path = get_libnacs_dir() + wfilename;
+    return LoadLibraryExW(path.data(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+}
+
+NACS_EXPORT() bool close(void *handle)
+{
+    if (!handle)
+        return false;
+    return FreeLibrary((HMODULE)handle);
+}
+
+NACS_EXPORT() void *sym(void *handle, const char *symbol)
+{
+    return (void*)GetProcAddress((HMODULE)handle, symbol);
+}
 #else
+NACS_EXPORT() void *open(const char *filename, Flags flags)
+{
 #  define CONVERT_FLAG(flags, FLAG) (flags & FLAG ? RTLD_ ## FLAG : 0)
     dlerror(); /* Reset error status. */
     return dlopen(filename, CONVERT_FLAG(flags, LOCAL) | CONVERT_FLAG(flags, GLOBAL) |
@@ -58,32 +135,22 @@ NACS_EXPORT() void *open(const char *filename, Flags flags)
                   CONVERT_FLAG(flags, FIRST) |
 #  endif
                   (flags & NOW ? RTLD_NOW : RTLD_LAZY));
-#endif
 }
 
 NACS_EXPORT() bool close(void *handle)
 {
-#if NACS_OS_WINDOWS
-    if (!handle)
-        return false;
-    return FreeLibrary((HMODULE)handle);
-#else
     dlerror(); /* Reset error status. */
     if (!handle)
         return false;
     return dlclose(handle) == 0;
-#endif
 }
 
 NACS_EXPORT() void *sym(void *handle, const char *symbol)
 {
-#if NACS_OS_WINDOWS
-    return GetProcAddress((HMODULE)handle, symbol);
-#else
     dlerror(); /* Reset error status. */
     return dlsym(handle, symbol);
-#endif
 }
+#endif
 
 }
 
