70a4539870bfc8225b259ffac531ef2441969698
Yichao Yu <yyc1992@gmail.com>
2019-03-05 22:05:41 -0500

    [utils/llvm] resolve vectorized math function to sleef functions.

--- lib/utils/llvm/execute.cpp
+++ lib/utils/llvm/execute.cpp
@@ -22,6 +22,8 @@
 #include "../dlload.h"
 #include "../processor.h"
 
+#include "config.h"
+
 #include <llvm/Support/MemoryBuffer.h>
 
 namespace NaCs {
@@ -40,6 +42,14 @@ namespace Exe {
 // Indirection to make sure `__COUNTER__` is expanded correctly.
 #define _asm_sym(var_suffix, name) __asm_sym(var_suffix, name)
 #define asm_sym(name) _asm_sym(__COUNTER__, name)
+#define __asm_sym_w(var_suffix, name)                                   \
+    ([] {                                                               \
+         extern void asm_sym ## var_suffix() asm(name) __attribute__((weak)); \
+         return asm_sym ## var_suffix;                                  \
+     }())
+// Indirection to make sure `__COUNTER__` is expanded correctly.
+#define _asm_sym_w(var_suffix, name) __asm_sym_w(var_suffix, name)
+#define asm_sym_w(name) _asm_sym_w(__COUNTER__, name)
 
 #ifdef ENABLE_SIMD
 #  if NACS_CPU_X86 || NACS_CPU_X86_64
@@ -50,10 +60,88 @@ namespace Exe {
 #    else
 #      define VEC_SUFFIX(x)
 #    endif
+#    define _check_sleef(host_info, sleef_sym_f, var, sym, s1, s2, s3) do { \
+        if (var == #sym ".2") {                                         \
+            if (host_info.test_feature(X86::Feature::avx2))             \
+                return sleef_sym_f(#sym "d2", "avx2128" VEC_SUFFIX(s1)); \
+            if (host_info.test_feature(X86::Feature::sse41))            \
+                return sleef_sym_f(#sym "d2", "sse4" VEC_SUFFIX(s1));   \
+            return sleef_sym_f(#sym "d2", "sse2" VEC_SUFFIX(s1));       \
+        }                                                               \
+        if (var == #sym ".4") {                                         \
+            if (host_info.test_feature(X86::Feature::avx2) &&           \
+                host_info.test_feature(X86::Feature::fma))              \
+                return sleef_sym_f(#sym "d4", "avx2" VEC_SUFFIX(s2));   \
+            if (host_info.test_feature(X86::Feature::fma4))             \
+                return sleef_sym_f(#sym "d4", "fma4" VEC_SUFFIX(s2));   \
+            return sleef_sym_f(#sym "d4", "avx" VEC_SUFFIX(s2));        \
+        }                                                               \
+        if (var == #sym ".8") {                                         \
+            return sleef_sym_f(#sym "d8", "avx512f" VEC_SUFFIX(s3));    \
+        }                                                               \
+    } while (0)
+#    define _check_sleef_d(host_info, sleef_sym_f, var, sym)    \
+    _check_sleef(host_info, sleef_sym_f, var, sym, 16, 32, 64)
+#    define _check_sleef_dd(host_info, sleef_sym_f, var, sym)   \
+    _check_sleef(host_info, sleef_sym_f, var, sym, 32, 64, 128)
+#    define _check_sleef_ddd(host_info, sleef_sym_f, var, sym)  \
+    _check_sleef(host_info, sleef_sym_f, var, sym, 48, 96, 192)
+#    define _check_sleef_di(host_info, sleef_sym_f, var, sym)   \
+    _check_sleef(host_info, sleef_sym_f, var, sym, 32, 48, 96)
 #  elif NACS_CPU_AARCH64
+#    define _check_sleef_d(host_info, sleef_sym_f, var, sym) do {       \
+        if (var == #sym ".2") {                                         \
+            return sleef_sym_f(#sym "d2", "advsimd");                   \
+        }                                                               \
+    } while (0)
+#    define _check_sleef_dd _check_sleef_d
+#    define _check_sleef_ddd _check_sleef_d
+#    define _check_sleef_di _check_sleef_d
 #  else
 #    error "SIMD not supported"
 #  endif
+#  define sleef_sym_u(prefix, suffix)                                   \
+    ([] {                                                               \
+         if (auto addr = asm_sym_w("Sleef_" prefix "_u35" suffix))      \
+             return (uintptr_t)addr;                                    \
+         return (uintptr_t)asm_sym("Sleef_" prefix "_u10" suffix);      \
+     } ())
+#  define sleef_sym_u15(prefix, suffix)                                 \
+    ([] {                                                               \
+         if (auto addr = asm_sym_w("Sleef_" prefix "_u35" suffix))      \
+             return (uintptr_t)addr;                                    \
+         return (uintptr_t)asm_sym("Sleef_" prefix "_u15" suffix);      \
+     } ())
+#  define sleef_sym_u05(prefix, suffix)                                 \
+    ([] {                                                               \
+         if (auto addr = asm_sym_w("Sleef_" prefix "_u35" suffix))      \
+             return (uintptr_t)addr;                                    \
+         return (uintptr_t)asm_sym("Sleef_" prefix "_u05" suffix);      \
+     } ())
+#  define sleef_sym(prefix, suffix)                                     \
+    ([] {                                                               \
+         if (auto addr = asm_sym_w("Sleef_" prefix "_u35" suffix))      \
+             return (uintptr_t)addr;                                    \
+         if (auto addr = asm_sym_w("Sleef_" prefix "_u05" suffix))      \
+             return (uintptr_t)addr;                                    \
+         return (uintptr_t)asm_sym("Sleef_" prefix "_" suffix);         \
+     } ())
+#  define check_sleef_u_d(host_info, var, sym)          \
+    _check_sleef_d(host_info, sleef_sym_u, var, sym)
+#  define check_sleef_u15_d(host_info, var, sym)        \
+    _check_sleef_d(host_info, sleef_sym_u15, var, sym)
+#  define check_sleef_d(host_info, var, sym)            \
+    _check_sleef_d(host_info, sleef_sym, var, sym)
+#  define check_sleef_u_dd(host_info, var, sym)         \
+    _check_sleef_dd(host_info, sleef_sym_u, var, sym)
+#  define check_sleef_u05_dd(host_info, var, sym)       \
+    _check_sleef_dd(host_info, sleef_sym_u05, var, sym)
+#  define check_sleef_dd(host_info, var, sym)           \
+    _check_sleef_dd(host_info, sleef_sym, var, sym)
+#  define check_sleef_ddd(host_info, var, sym)          \
+    _check_sleef_ddd(host_info, sleef_sym, var, sym)
+#  define check_sleef_di(host_info, var, sym)           \
+    _check_sleef_di(host_info, sleef_sym, var, sym)
 #endif
 
 JITSymbol Resolver::findSymbolInLogicalDylib(const std::string&)
@@ -81,9 +169,9 @@ uintptr_t Resolver::find_extern(const std::string &name)
         return (uintptr_t)nacs_exp10;
 #endif
 #ifdef ENABLE_SIMD
+    static const auto &host_info = CPUInfo::get_host();
 #  if NACS_CPU_X86 || NACS_CPU_X86_64
     // We need this to convience gcc to find the correct symbol on windows.
-    static const auto &host_info = CPUInfo::get_host();
     if (name == "interp.2") {
         auto addr = asm_sym("_ZN4NaCs23linearInterpolate2_sse2EDv2_djPKd" VEC_SUFFIX(32));
         if (host_info.test_feature(X86::Feature::avx2))
@@ -104,6 +192,7 @@ uintptr_t Resolver::find_extern(const std::string &name)
     }
 #    endif
 #  elif NACS_CPU_AARCH64
+    (void)host_info;
     if (name == "interp.2") {
         return (uintptr_t)static_cast<float64x2_t(*)(float64x2_t, uint32_t, const double*)>(
             linearInterpolate2_asimd);
@@ -117,6 +206,49 @@ uintptr_t Resolver::find_extern(const std::string &name)
             linearInterpolate8_asimd);
     }
 #  endif
+    check_sleef_u_d(host_info, name, acos);
+    check_sleef_u_d(host_info, name, acosh);
+    check_sleef_u_d(host_info, name, asin);
+    check_sleef_u_d(host_info, name, asinh);
+    check_sleef_u_d(host_info, name, atan);
+    check_sleef_u_d(host_info, name, atanh);
+    check_sleef_u_d(host_info, name, cbrt);
+    check_sleef_d(host_info, name, ceil);
+    check_sleef_u_d(host_info, name, cos);
+    check_sleef_u_d(host_info, name, cosh);
+    check_sleef_u_d(host_info, name, erf);
+    check_sleef_u15_d(host_info, name, erfc);
+    check_sleef_u_d(host_info, name, exp);
+    check_sleef_u_d(host_info, name, exp10);
+    check_sleef_u_d(host_info, name, exp2);
+    check_sleef_u_d(host_info, name, expm1);
+    check_sleef_d(host_info, name, fabs);
+    check_sleef_d(host_info, name, floor);
+    check_sleef_u_d(host_info, name, tgamma);
+    check_sleef_u_d(host_info, name, lgamma);
+    check_sleef_u_d(host_info, name, log);
+    check_sleef_u_d(host_info, name, log10);
+    check_sleef_u_d(host_info, name, log1p);
+    check_sleef_u_d(host_info, name, log2);
+    check_sleef_d(host_info, name, rint);
+    check_sleef_d(host_info, name, round);
+    check_sleef_u_d(host_info, name, sin);
+    check_sleef_u_d(host_info, name, sinh);
+    check_sleef_d(host_info, name, sqrt);
+    check_sleef_u_d(host_info, name, tan);
+    check_sleef_u_d(host_info, name, tanh);
+
+    check_sleef_u_dd(host_info, name, atan2);
+    check_sleef_dd(host_info, name, copysign);
+    check_sleef_dd(host_info, name, fdim);
+    check_sleef_dd(host_info, name, fmax);
+    check_sleef_dd(host_info, name, fmin);
+    check_sleef_dd(host_info, name, fmod);
+    check_sleef_u05_dd(host_info, name, hypot);
+    check_sleef_u_dd(host_info, name, pow);
+
+    check_sleef_ddd(host_info, name, fma);
+    check_sleef_di(host_info, name, ldexp);
 #endif
     return (uintptr_t)DL::sym(nullptr, name.c_str());
 }
