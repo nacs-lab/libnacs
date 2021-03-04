8d318d3fcd5b8f1124320551ff7e7ef79674bee3
Yichao Yu <yyc1992@gmail.com>
2021-03-04 14:27:28 -0500

    [utils/utils] std::experimental::is_detected from Library Fundamentals TS v2

--- lib/utils/utils.h
+++ lib/utils/utils.h
@@ -205,6 +205,54 @@ static inline constexpr decltype(auto) applyTuple(F&& f, Tuple&& t)
                            std::tuple_size<std::decay_t<Tuple>>::value>{});
 }
 
+// Library Fundamentals TS v2. Most likely replaced by concept in C++20.
+namespace detail {
+template <class Default, class AlwaysVoid,
+          template<class...> class Op, class... Args>
+struct detector {
+    using value_t = std::false_type;
+    using type = Default;
+};
+
+template <class Default, template<class...> class Op, class... Args>
+struct detector<Default, std::void_t<Op<Args...>>, Op, Args...> {
+    using value_t = std::true_type;
+    using type = Op<Args...>;
+};
+
+struct nonesuch {
+    ~nonesuch() = delete;
+    nonesuch(nonesuch const&) = delete;
+    void operator=(nonesuch const&) = delete;
+};
+
+} // namespace detail
+
+template <template<class...> class Op, class... Args>
+using is_detected = typename detail::detector<detail::nonesuch, void, Op, Args...>::value_t;
+
+template <template<class...> class Op, class... Args>
+using detected_t = typename detail::detector<detail::nonesuch, void, Op, Args...>::type;
+
+template <class Default, template<class...> class Op, class... Args>
+using detected_or = detail::detector<Default, void, Op, Args...>;
+
+template< template<class...> class Op, class... Args >
+constexpr inline bool is_detected_v = is_detected<Op, Args...>::value;
+template< class Default, template<class...> class Op, class... Args >
+using detected_or_t = typename detected_or<Default, Op, Args...>::type;
+
+template <class Expected, template<class...> class Op, class... Args>
+using is_detected_exact = std::is_same<Expected, detected_t<Op, Args...>>;
+template <class Expected, template<class...> class Op, class... Args>
+constexpr inline bool is_detected_exact_v = is_detected_exact<Expected, Op, Args...>::value;
+
+template <class To, template<class...> class Op, class... Args>
+using is_detected_convertible = std::is_convertible<detected_t<Op, Args...>, To>;
+template <class To, template<class...> class Op, class... Args>
+constexpr inline bool is_detected_convertible_v =
+    is_detected_convertible<To, Op, Args...>::value;
+
 }
 
 #endif
