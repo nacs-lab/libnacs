f459755f85e52d48a849edaaaae7f65b24677a7c
Yichao Yu <yyc1992@gmail.com>
2021-03-22 09:42:25 -0400

    [test] Helper to catch and examine errors

---
+++ test/error_helper.h
@@ -0,0 +1,37 @@
+/*************************************************************************
+ *   Copyright (c) 2021 - 2021 Yichao Yu <yyc1992@gmail.com>             *
+ *                                                                       *
+ *   This library is free software; you can redistribute it and/or       *
+ *   modify it under the terms of the GNU Lesser General Public          *
+ *   License as published by the Free Software Foundation; either        *
+ *   version 3.0 of the License, or (at your option) any later version.  *
+ *                                                                       *
+ *   This library is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU    *
+ *   Lesser General Public License for more details.                     *
+ *                                                                       *
+ *   You should have received a copy of the GNU Lesser General Public    *
+ *   License along with this library. If not,                            *
+ *   see <http://www.gnu.org/licenses/>.                                 *
+ *************************************************************************/
+
+#ifndef _NACS_TEST_ERROR_HELPER_H_
+#define _NACS_TEST_ERROR_HELPER_H_
+
+#include <assert.h>
+
+template<typename T, typename CB>
+static T expect_error(CB &&cb)
+{
+    try {
+        cb();
+        assert(false);
+    }
+    catch (const T &err) {
+        return err;
+    }
+    assert(false);
+}
+
+#endif
