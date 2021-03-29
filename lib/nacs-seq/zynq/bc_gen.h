b77542c90336f52c39f7d67a8d58be3f2926c8df
Yichao Yu <yyc1992@gmail.com>
2021-03-29 07:52:59 -0400

    [seq/zynq/bc_gen] Bytecode generator for the Zynq backend of the new sequence implementation.
    This is slightly simplified from the legacy implementation by dividing the work into more passes.
    For example, the rate limitation on the pulses are now done before we start scheduling the pulses
    and so is the conversion from clock period to clock pulse times.
    
    We also moved TTL manager (which applies a global transformation to the TTL pulse times)
    into the backend since there's basically no way we can do this in the MATLAB code anymore
    without the full information about the timing.
    
    The ramp function can now be vectorized functions,
    in which case we use the current pulse spacing to predict when the next sample(s) is(are)
    and cache the result of multiple time points.

---
+++ lib/nacs-seq/zynq/bc_gen.h
@@ -0,0 +1,191 @@
+/*************************************************************************
+ *   Copyright (c) 2021 - 2021 Yichao Yu <yyc1992@gmail.com>             *
+ *                                                                       *
+ *   This library is free software; you can redistribute it and/or       *
+ *   modify it under the terms of the GNU Lesser General Public          *
+ *   License as published by the Free Software Foundation; either        *
+ *   version 3.0 of the License, or (at your option) any later version.  *
+ *                                                                       *
+ *   This library is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU    *
+ *   Lesser General Public License for more details.                     *
+ *                                                                       *
+ *   You should have received a copy of the GNU Lesser General Public    *
+ *   License along with this library. If not,                            *
+ *   see <http://www.gnu.org/licenses/>.                                 *
+ *************************************************************************/
+
+#ifndef __NACS_SEQ_ZYNQ_BC_GEN_H__
+#define __NACS_SEQ_ZYNQ_BC_GEN_H__
+
+#include "../host_seq.h"
+
+#include <nacs-utils/number.h>
+
+#include <map>
+#include <set>
+#include <vector>
+
+#include <assert.h>
+
+namespace NaCs::Seq::Zynq {
+
+class BCGen {
+    struct Pulse;
+    struct TTLPulse;
+    struct TTLManager;
+    struct Writer;
+    struct DataStream;
+
+public:
+    enum class ChnType : uint8_t {
+        TTL = 1,
+        Freq = 2,
+        Amp = 3,
+        DAC = 4,
+    };
+    enum class PulseType : uint8_t {
+        Value,
+        Scalar,
+        Vector,
+    };
+    struct SeqPulse {
+        ChnType chn_type;
+        PulseType pulse_type;
+        uint8_t chn;
+
+        uint32_t id;
+        uint32_t time_id;
+        uint32_t len_id; // may be -1, `len == -1` <=> no-ramp pulse
+        uint32_t endvalue_id;
+        uint32_t cond_id; // may be -1
+        // double (*)(double in, const void *data) for scalar function
+        // void (*)(double *out, const double *in, const void *data) for vector function
+        // Use reference passing for vector function since GCC doesn't support
+        // vector calling convention on windows
+        // and clang doesn't support per-function vector calling convention.
+        void (*ramp_func)(void);
+    };
+    struct Clock {
+        int64_t time; // in sequence time unit
+        uint8_t period;
+    };
+
+    BCGen();
+    ~BCGen();
+    void generate(HostSeq &host_seq);
+    static uint32_t convert_value(ChnType type, double value);
+    void add_ttl_manager(uint8_t chn, uint32_t off_delay, uint32_t on_delay,
+                         uint32_t skip_time, uint32_t min_time, bool off_val);
+    static uint32_t version();
+
+    // Inputs
+    uint32_t seq_idx;
+    uint32_t seq_delay; // in unit of 10ns.
+    std::vector<SeqPulse> seq_pulses;
+    // Assume this includes all the channels needed.
+    // For `first_bseq`, this is needed for all channels.
+    // For non-`first_bseq`, this is needed only for TTL pulses.
+    // In both cases, this is expected to be the value without correcting for t=0 pulses.
+    std::map<std::pair<ChnType,uint8_t>,uint32_t> start_vals;
+    std::vector<Clock> clocks;
+    bool first_bseq;
+
+    int8_t start_ttl_chn;
+    uint32_t ttl_mask;
+    // How many sequence cycle corresponds to one FPGA clock cycle.
+    uint32_t fpga_clock_div;
+    int64_t len_ns;
+
+    // Output
+    std::vector<uint8_t> bytecode;
+
+private:
+    template<typename T>
+    struct ChnMap {
+        static constexpr int chn_num = 22 + 22 + 4;
+        T channels[chn_num];
+
+        template<typename T2>
+        void fill(T2 v)
+        {
+            for (auto &chn: channels) {
+                chn = v;
+            }
+        }
+        static std::pair<BCGen::ChnType,uint8_t> to_channel(int idx)
+        {
+            if (idx < 22)
+                return {BCGen::ChnType::Freq, idx};
+            idx -= 22;
+            if (idx < 22)
+                return {BCGen::ChnType::Amp, idx};
+            idx -= 22;
+            return {BCGen::ChnType::DAC, idx};
+        }
+        T &operator[](std::pair<BCGen::ChnType,uint8_t> chn)
+        {
+            if (chn.first == BCGen::ChnType::Freq) {
+                assert(chn.second < 22);
+                return channels[chn.second];
+            }
+            else if (chn.first == BCGen::ChnType::Amp) {
+                assert(chn.second < 22);
+                return channels[chn.second + 22];
+            }
+            else if (chn.first == BCGen::ChnType::DAC) {
+                assert(chn.second < 4);
+                return channels[chn.second + 22 * 2];
+            }
+            assert(false && "Invalid channel type.");
+            abort();
+        }
+        T &operator[](int idx)
+        {
+            assert(idx < chn_num);
+            return channels[idx];
+        }
+        T *begin()
+        {
+            return channels;
+        }
+        T *end()
+        {
+            return &channels[chn_num];
+        }
+        const T *begin() const
+        {
+            return channels;
+        }
+        const T *end() const
+        {
+            return &channels[chn_num];
+        }
+    };
+
+    int64_t convert_time(int64_t seq_time) const
+    {
+        seq_time += seq_time >= 0 ? int32_t(fpga_clock_div) / 2 : -int32_t(fpga_clock_div) / 2;
+        return seq_time / fpga_clock_div;
+    }
+    int64_t convert_time(double seq_time) const
+    {
+        return round<int64_t>(seq_time / fpga_clock_div);
+    }
+    void populate_pulses(HostSeq &host_seq);
+    bool preprocess_ttl_pulse(const TTLPulse &ttl_pulse);
+    void preprocess_ttl_managers();
+    void merge_pulses();
+    void merge_ttl_pulses();
+
+    void emit_bytecode(void *data);
+
+    std::vector<TTLManager> m_ttl_managers;
+    ChnMap<std::vector<Pulse>> m_pulses;
+    std::vector<TTLPulse> m_ttlpulses;
+};
+
+}
+
+#endif
---
+++ test/seq/zynq_helper.h
@@ -0,0 +1,107 @@
+/*************************************************************************
+ *   Copyright (c) 2021 - 2021 Yichao Yu <yyc1992@gmail.com>             *
+ *                                                                       *
+ *   This library is free software; you can redistribute it and/or       *
+ *   modify it under the terms of the GNU Lesser General Public          *
+ *   License as published by the Free Software Foundation; either        *
+ *   version 3.0 of the License, or (at your option) any later version.  *
+ *                                                                       *
+ *   This library is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU    *
+ *   Lesser General Public License for more details.                     *
+ *                                                                       *
+ *   You should have received a copy of the GNU Lesser General Public    *
+ *   License along with this library. If not,                            *
+ *   see <http://www.gnu.org/licenses/>.                                 *
+ *************************************************************************/
+
+#ifndef __NACS_TEST_SEQ_ZYNQ_HELPER_H__
+#define __NACS_TEST_SEQ_ZYNQ_HELPER_H__
+
+#include "../../lib/nacs-seq/zynq/bc_gen.h"
+#include "../../lib/nacs-seq/zynq/bytecode.h"
+
+#include "../../lib/nacs-utils/number.h"
+#include "../../lib/nacs-utils/processor.h"
+
+#include <sstream>
+
+#include <catch2/catch.hpp>
+
+using namespace NaCs;
+using namespace NaCs::Seq;
+
+namespace {
+
+static unsigned vector_size = [] {
+#if NACS_CPU_X86 || NACS_CPU_X86_64
+    auto &host = CPUInfo::get_host();
+    if (host.test_feature(X86::Feature::avx512f) &&
+        host.test_feature(X86::Feature::avx512dq)) {
+        return 8;
+    }
+    else if (host.test_feature(X86::Feature::avx)) {
+        return 4;
+    }
+    else {
+        return 2;
+    }
+#elif NACS_CPU_AARCH64
+    // TODO SVE ...
+    return 2;
+#else
+    return 1;
+#endif
+}();
+
+NACS_UNUSED static auto dump_bytecode(const uint8_t *bytecode, size_t size)
+{
+    uint64_t len;
+    memcpy(&len, bytecode, sizeof(uint64_t));
+    bytecode += sizeof(uint64_t);
+    size -= sizeof(uint64_t);
+    uint32_t ttl_mask;
+    memcpy(&ttl_mask, bytecode, sizeof(uint32_t));
+    std::stringstream stm;
+    stm << "# len_ns=" << len << std::endl;
+    stm << "ttl_mask=0x" << std::hex << ttl_mask << std::dec << std::endl;
+    Zynq::ByteCode::print_raw(stm, bytecode + sizeof(uint32_t),
+                              size - sizeof(uint32_t), Zynq::BCGen::version());
+    return stm.str();
+}
+
+NACS_UNUSED static auto dump_bytecode(const std::vector<uint8_t> &bytecode)
+{
+    return dump_bytecode(bytecode.data(), bytecode.size());
+}
+
+struct Checker {
+    Checker(const uint8_t *data, size_t size)
+        : data(data),
+          size(size)
+    {}
+    Checker(const std::vector<uint8_t> &data)
+        : Checker(data.data(), data.size())
+    {}
+    template<typename T>
+    bool cmp(T v)
+    {
+        REQUIRE(sizeof(T) <= size);
+        bool res = memcmp(data, &v, sizeof(T)) == 0;
+        data += sizeof(T);
+        size -= sizeof(T);
+        return res;
+    }
+    bool is_end() const
+    {
+        return size == 0;
+    }
+
+    const uint8_t *data;
+    size_t size;
+};
+
+}
+
+#endif
