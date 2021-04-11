3750f0e8acf39784393da7fe5feea52b0f3b1583
Yichao Yu <yyc1992@gmail.com>
2021-04-11 14:36:27 -0400

    [cmake,utils/zmq] Add async ZMQ client
    Use a DEALER socket to allow simultaneously sending multiple messages.
    The user can use either a callback or a `std::future` to get the result.
    Find zmq using pkg-config.

---
+++ lib/utils/zmq_utils.cpp
@@ -0,0 +1,163 @@
+/*************************************************************************
+ *   Copyright (c) 2021 - 2021 Yichao Yu <yyc1992@gmail.com>             *
+ *                                                                       *
+ *   This library is free software; you can redistribute it and/or       *
+ *   modify it under the terms of the GNU Lesser General Public          *
+ *   License as published by the Free Software Foundation; either        *
+ *   version 3.0 of the License, or (at your option) any later version.  *
+ *                                                                       *
+ *   This library is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU    *
+ *   Lesser General Public License for more details.                     *
+ *                                                                       *
+ *   You should have received a copy of the GNU Lesser General Public    *
+ *   License along with this library. If not,                            *
+ *   see <http://www.gnu.org/licenses/>.                                 *
+ *************************************************************************/
+
+#include "zmq_utils.h"
+
+namespace NaCs::ZMQ {
+
+NACS_EXPORT() zmq::context_t &global_context()
+{
+    static zmq::context_t context;
+    return context;
+}
+
+NACS_EXPORT() MultiClient &MultiClient::global()
+{
+    static MultiClient multi_client(global_context());
+    return multi_client;
+}
+
+NACS_EXPORT_ MultiClient::MultiClient(zmq::context_t &context)
+    : m_context(context)
+{
+}
+
+NACS_EXPORT() MultiClient::~MultiClient()
+{
+    if (m_worker.joinable()) {
+        std::unique_lock<std::mutex> locker(m_lock);
+        m_sockets.clear();
+        locker.unlock();
+        m_worker.join();
+    }
+}
+
+NACS_EXPORT() uint64_t MultiClient::SockRef::send_addr()
+{
+    // Assume the client lock is held.
+    uint64_t id = ++m_info.msg_counter;
+    send_more(m_info.sock, bits_msg(id));
+    send_more(m_info.sock, m_info.empty);
+    return id;
+}
+
+NACS_EXPORT() void MultiClient::SockRef::add_wait(uint64_t id, finish_cb_t finish_cb)
+{
+    // Assume the client lock is held.
+    m_info.callbacks.emplace(id, finish_cb);
+}
+
+NACS_EXPORT() MultiClient::SockRef MultiClient::get_socket(const std::string &addr)
+{
+    std::lock_guard<std::mutex>locker(m_lock);
+    auto it = m_sockets.find(addr);
+    if (it == m_sockets.end()) {
+        it = m_sockets.try_emplace(addr, *this).first;
+        auto &info = it->second;
+        info.sock.setsockopt(ZMQ_LINGER, int(0));
+        info.sock.connect(addr);
+        ensure_worker();
+    }
+    return SockRef(it->second);
+}
+
+void MultiClient::ensure_worker()
+{
+    // Assume the client lock is held.
+    if (m_running)
+        return;
+    if (m_worker.joinable())
+        m_worker.join();
+    m_running = true;
+    m_worker = std::thread(&MultiClient::worker_func, this);
+}
+
+void MultiClient::worker_func()
+{
+    std::unique_lock<std::mutex> locker(m_lock);
+    // TODO: use poller when it's finalized.
+    std::vector<zmq::pollitem_t> poll_items;
+    std::vector<decltype(m_sockets.begin())> sockets;
+    zmq::message_t msg;
+    std::vector<zmq::message_t> msgs;
+    while (true) {
+        poll_items.clear();
+        // Cache the socket since other thread may have added new ones when we are polling.
+        sockets.clear();
+        for (auto it = m_sockets.begin(), end = m_sockets.end(); it != end;) {
+            auto &info = it->second;
+            if (info.ref_count == 0) {
+                it = m_sockets.erase(it);
+            }
+            else {
+                poll_items.push_back({(void*)info.sock, 0, ZMQ_POLLIN, 0});
+                sockets.push_back(it);
+                ++it;
+            }
+        }
+        if (poll_items.empty())
+            break;
+        locker.unlock();
+        // 15ms timeout. This is the maximum delay when we add a new socket.
+        zmq::poll(poll_items, 15);
+        locker.lock();
+        auto nsockets = sockets.size();
+        for (size_t i = 0; i < nsockets; i++) {
+            auto &item = poll_items[i];
+            if (!item.revents)
+                continue;
+            assert(item.revents == ZMQ_POLLIN);
+            auto &info = sockets[i]->second;
+            recv(info.sock, msg);
+            if (msg.size() != 8) {
+                readall(info.sock);
+                continue;
+            }
+            uint64_t id;
+            memcpy(&id, msg.data(), 8);
+            if (!recv_more(info.sock, msg))
+                continue;
+            // Make sure we get the delimiter frame.
+            if (msg.size() != 0) {
+                readall(info.sock);
+                continue;
+            }
+            auto cb_it = info.callbacks.find(id);
+            if (cb_it == info.callbacks.end()) {
+                readall(info.sock);
+                continue;
+            }
+            auto cb = cb_it->second;
+            info.callbacks.erase(cb_it);
+            while (recv_more(info.sock, msg))
+                msgs.push_back(std::move(msg));
+            locker.unlock();
+            try {
+                cb(nullptr, std::move(msgs));
+            }
+            catch (...) {
+                // Ignore
+            }
+            msgs.clear();
+            locker.lock();
+        }
+    }
+    m_running = false;
+}
+
+}
--- lib/utils/zmq_utils.h
+++ lib/utils/zmq_utils.h
@@ -1,5 +1,5 @@
 /*************************************************************************
- *   Copyright (c) 2018 - 2018 Yichao Yu <yyc1992@gmail.com>             *
+ *   Copyright (c) 2018 - 2021 Yichao Yu <yyc1992@gmail.com>             *
  *                                                                       *
  *   This library is free software; you can redistribute it and/or       *
  *   modify it under the terms of the GNU Lesser General Public          *
@@ -21,6 +21,14 @@
 #include <zmq.hpp>
 #include <string.h>
 
+#include <atomic>
+#include <functional>
+#include <future>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
 #include <vector>
 
 #ifndef __NACS_UTILS_ZMQ_H__
@@ -74,12 +82,12 @@ static inline void send_addr(zmq::socket_t &sock, std::vector<zmq::message_t> &a
     send_addr(sock, addr, empty);
 }
 
-static inline void recv(zmq::socket_t &sock, zmq::message_t &msg)
+static inline auto recv(zmq::socket_t &sock, zmq::message_t &msg)
 {
 #if CPPZMQ_VERSION >= 40301
-    sock.recv(msg);
+    return sock.recv(msg);
 #else
-    sock.recv(&msg);
+    return sock.recv(&msg);
 #endif
 }
 
@@ -143,6 +151,89 @@ static inline zmq::message_t str_msg(const char *str)
 {
     return zmq::message_t(str, strlen(str));
 }
+
+NACS_EXPORT(utils) zmq::context_t &global_context();
+
+class MultiClient {
+    using finish_cb_t = std::function<void(std::exception_ptr,std::vector<zmq::message_t>)>;
+    struct SocketInfo {
+        SocketInfo(MultiClient &client)
+            : sock(client.m_context, ZMQ_DEALER),
+              client(client)
+        {
+        }
+        zmq::socket_t sock;
+        MultiClient &client;
+        std::atomic<uint32_t> ref_count{0};
+        uint64_t msg_counter = 0;
+        std::map<uint64_t,finish_cb_t> callbacks{};
+        zmq::message_t empty{};
+    };
+
+public:
+    NACS_EXPORT(utils) MultiClient(zmq::context_t &context);
+    NACS_EXPORT(utils) ~MultiClient();
+    NACS_EXPORT(utils) static MultiClient &global();
+    class SockRef {
+    public:
+        SockRef(SocketInfo &info)
+            : m_info(info)
+        {
+            info.ref_count.fetch_add(1, std::memory_order_relaxed);
+        }
+        SockRef(const SockRef &other)
+            : SockRef(other.m_info)
+        {
+        }
+        ~SockRef()
+        {
+            assert(m_info.ref_count.load(std::memory_order_relaxed) >= 1);
+            m_info.ref_count.fetch_add(-1, std::memory_order_relaxed);
+        }
+        // Send function should not throw.
+        template<typename SendFunc>
+        void send_msg(SendFunc &&send_func, finish_cb_t finish_cb)
+        {
+            std::lock_guard<std::mutex> locker(m_info.client.m_lock);
+            auto id = send_addr();
+            std::forward<SendFunc>(send_func)(m_info.sock);
+            add_wait(id, finish_cb);
+        }
+        template<typename SendFunc>
+        std::future<std::vector<zmq::message_t>> send_msg(SendFunc &&send_func)
+        {
+            // Use a shared pointer to work around std::function's requirement on copyable.
+            auto promise = std::make_shared<std::promise<std::vector<zmq::message_t>>>();
+            send_msg(std::forward<SendFunc>(send_func),
+                     [promise] (auto except, auto res) mutable {
+                         if (except) {
+                             promise->set_exception(except);
+                         }
+                         else {
+                             promise->set_value(std::move(res));
+                         }
+                     });
+            return promise->get_future();
+        }
+
+    private:
+        NACS_EXPORT(utils) uint64_t send_addr();
+        NACS_EXPORT(utils) void add_wait(uint64_t id, finish_cb_t finish_cb);
+
+        SocketInfo &m_info;
+    };
+    NACS_EXPORT(utils) SockRef get_socket(const std::string &addr);
+
+private:
+    void ensure_worker();
+    void worker_func();
+
+    zmq::context_t &m_context;
+    std::map<std::string,SocketInfo> m_sockets;
+    std::mutex m_lock;
+    std::thread m_worker;
+    bool m_running;
+};
 
 }
 
