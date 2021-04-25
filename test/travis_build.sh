1104b1ed5e0e8265bdeb65edc59ec9c6b27c6176
Yichao Yu <yyc1992@gmail.com>
2021-04-25 17:53:53 -0400

    [travis] no llvm build

--- test/travis_build.sh
+++ test/travis_build.sh
@@ -2,5 +2,9 @@
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_LLVM=On -DENABLE_SIMD=On
+if [ -n "${LLVM_VER}" ]; then
+    cmake .. -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_LLVM=On -DENABLE_SIMD=On
+else
+    cmake .. -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_SIMD=On
+fi
 make -j $(nproc)
