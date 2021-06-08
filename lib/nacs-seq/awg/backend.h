3eaee8fea635bef8b3ef69ddc0f07ebf74622b8f
Ni Lab <niharvardlab@gmail.com>
2021-06-08 16:30:30 +0000

    backend compiles!

--- lib/nacs-seq/awg/backend.h
+++ lib/nacs-seq/awg/backend.h
@@ -7,12 +7,43 @@
 
 #include <map>
 #include <vector>
+#include <memory>
+
+#include <nacs-utils/zmq_utils.h>
+
 
 namespace NaCs::Seq {
 
+namespace Zynq {
+class Backend;
+}
+
 namespace AWG {
 
+//globals for all AWG backends
+uint64_t seqcount = 0;
+
+struct GlobalInfo {
+    uint64_t client_id;
+    uint64_t server_id;
+    std::string triple_str;
+    std::string cpu_str;
+    std::string feature_str;
+    GlobalInfo(uint64_t client, uint64_t server, std::string triple,
+               std::string cpu, std::string feature):
+        client_id(client),
+        server_id(server),
+        triple_str(triple),
+        cpu_str(cpu),
+        feature_str(feature)
+    {
+    }
+};
+
+std::map <std::string, GlobalInfo> info_map;
+
 class Backend : public Device {
+    enum class ChnType : uint8_t;
     struct BasicSeq;
     struct ChannelInfo;
 
@@ -26,12 +57,13 @@ private:
     void prepare (Manager::ExpSeq &expseq, Compiler &compiler) override;
     void generate (Manager::ExpSeq &expseq, Compiler &compiler) override;
 
+    void init_run(HostSeq &host_seq) override;
     void prepare_run(HostSeq &host_seq) override;
     void pre_run(HostSeq &host_seq) override;
     void start(HostSeq &host_seq) override;
     void cancel(HostSeq &host_seq) override;
     void wait(HostSeq &host_seq) override;
-
+    void post_run(HostSeq &host_seq) override;
     // void config(const YAML::Node&) override;
 
     void sort_channels();
@@ -39,6 +71,10 @@ private:
     bool pregenerate(Manager::ExpSeq &expseq, Compiler &compiler, uint32_t seq_idx);
     void populate_values(HostSeq &host_seq, BasicSeq &bseq);
 
+
+    uint8_t get_pulse_type(Backend::ChnType type, bool is_fn, bool is_vector);
+    void get_chn_from_lin(uint32_t lin_idx, Backend::ChnType &type, uint8_t &phys_chn_id, uint32_t &chn_id);
+    void config (const YAML::Node&) override;
     std::vector<BasicSeq> m_seqs;
     // Map from sequence channel ID to physical device and channel name/ids etc.
     std::map<uint32_t,ChannelInfo> m_chn_map;
@@ -48,10 +84,22 @@ private:
     uint32_t m_nconst_vals;
     bool m_first_bseq = false;
     uint32_t m_cur_seq_id = 0;
+    uint64_t m_cur_wait_id = 0;
     uint64_t m_obj_id = 0;
+    uint64_t m_seq_cnt_offset = 0;
     // Serialized channel info to make passing it to python/MATLAB easier
     std::vector<uint8_t> m_channel_info;
-
+    std::string m_clock_dev;
+    Zynq::Backend *m_zynq_dev = nullptr;
+    // network related variables...
+    std::string m_url;
+    std::unique_ptr<ZMQ::MultiClient::SockRef> m_sock;
+    uint64_t m_client_id;
+    uint64_t m_server_id;
+    std::string m_triple_str;
+    std::string m_cpu_str;
+    std::string m_feature_str;
+    std::vector<uint8_t> id_bc;
 };
 
 }
