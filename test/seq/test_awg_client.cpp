9c1b72ae78ab8b28869f67040c37c69e0707fbe3
Ni Lab <niharvardlab@gmail.com>
2021-06-10 14:12:11 +0000

    seg faulting code....

---
+++ test/seq/test_awg_client.cpp
@@ -0,0 +1,21 @@
+//
+
+#define LLVM_DISABLE_ABI_BREAKING_CHECKS_ENFORCING 1
+
+//#include "DummyClient.h"
+#include <llvm/IR/LLVMContext.h>
+#include "../../lib/nacs-seq/awg/DummyClient.h"
+
+#include <iostream>
+
+using namespace NaCs;
+int main ()
+{
+    std::string fname = "/etc/client_config.yml";
+    std::cout << "here" << std::endl;
+    Seq::AWG::Dummy::DummyClient client;
+    //client.loadConfig(fname.data());
+    //std::cout << client.m_url << std::endl;
+    // test network
+    
+}
