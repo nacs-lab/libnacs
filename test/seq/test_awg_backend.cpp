a352a94195d121ac8fa3c01d7ff447591da4c5b3
Ni Lab <niharvardlab@gmail.com>
2021-08-03 21:36:16 +0000

    fix global bugs, get mult output working

--- test/seq/test_awg_backend.cpp
+++ test/seq/test_awg_backend.cpp
@@ -8,6 +8,8 @@
 #include "../../lib/nacs-seq/manager.h"
 #include "../../lib/nacs-seq/awg/backend.h"
 #include "../../lib/nacs-utils/streams.h"
+#include <iostream>
+
 using namespace NaCs;
 using namespace NaCs::Seq;
 using namespace std::literals::string_literals;
@@ -396,23 +398,23 @@ void test_set_pulse_global() {
     // 1
     stm.write(Builder::OpCode::Identity);
     stm.write(Builder::ArgType::Global);
-    stm.write<double>(0);
+    stm.write<uint32_t>(0);
     // 2
     stm.write(Builder::OpCode::Identity);
     stm.write(Builder::ArgType::Global);
-    stm.write<double>(1);
+    stm.write<uint32_t>(1);
     // 3
     stm.write(Builder::OpCode::Identity);
     stm.write(Builder::ArgType::Global);
-    stm.write<double>(2);
+    stm.write<uint32_t>(2);
     // 4
     stm.write(Builder::OpCode::Identity);
     stm.write(Builder::ArgType::Global);
-    stm.write<double>(3);
+    stm.write<uint32_t>(3);
     // 5
     stm.write(Builder::OpCode::Identity);
     stm.write(Builder::ArgType::Global);
-    stm.write<double>(4);
+    stm.write<uint32_t>(4);
     // 6
     stm.write(Builder::OpCode::Identity);
     stm.write(Builder::ArgType::ConstFloat64);
@@ -475,7 +477,7 @@ void test_set_pulse_global() {
         // [ntimeorders: 4B][[sign: 1B][id: 4B][before_id: 4B][after_id: 4B] x ntimeorders]
         stm.write<uint32_t>(0);
         // [noutputs: 4B][[id: 4B][time_id: 4B][len: 4B][val: 4B][cond: 4B][chn: 4B] x noutputs]
-        stm.write<uint32_t>(3);
+        stm.write<uint32_t>(4);
         // 1 12 ms after start, set chn1 freq to global 0
         stm.write<uint32_t>(8); // id
         stm.write<uint32_t>(1); // time_id
@@ -521,21 +523,29 @@ void test_set_pulse_global() {
 
     auto expseq = stm.create_sequence();
 
+    printf(expseq->host_seq.first_bseq ? "first_bseq" : "not_first_bseq");
     expseq->init_run();
-    expseq->host_seq.set_global(0, 60e6);
+    expseq->host_seq.set_global(0, 4e3);
     expseq->host_seq.set_global(1, 0.4);
-    expseq->host_seq.set_global(2, 40e6);
+    expseq->host_seq.set_global(2, 5e3);
     expseq->host_seq.set_global(3, 0.1);
     expseq->host_seq.set_global(4, 0); // conditionally turn off chn 2 pulse
     expseq->pre_run();
     expseq->start();
     expseq->wait(60000);
+    expseq->post_run();
+    printf("Press any key to continue");
+    int flag = std::cin.get();
 
-    expseq->host_seq.set_global(0, 70e6);
+    //printf("Is first bseq: %d\n", expseq->host_seq.first_bseq);
+    printf(expseq->host_seq.first_bseq ? "first_bseq" : "not_first_bseq");
+    expseq->host_seq.set_global(0, 3e3);
     expseq->host_seq.set_global(4, 1);
+    expseq->init_run();
     expseq->pre_run();
     expseq->start();
     expseq->wait(60000);
+    expseq->post_run();
 }
 
 void test_ramp_pulse_global() {
@@ -792,10 +802,174 @@ void test_ramp_pulse_global() {
     stm.write<uint32_t>(0);
 
     auto expseq = stm.create_sequence();
+    // TODO
+}
+
+void test_set_pulse_global_mult() {
+    // test multiple channel output
+    ostream stm;
+    // [version <0>: 1B]
+    stm.write<uint8_t>(0);
+    // [nnodes: 4B]
+    // [[[OpCode: 1B][[ArgType: 1B][NodeArg: 1-8B] x narg] /
+    //   [OpCode <Interp> : 1B][[ArgType: 1B][NodeArg: 1-8B] x 3][data_id: 4B]] x nnodes]//
+    stm.write<uint32_t>(8);
+    // 1
+    stm.write(Builder::OpCode::Identity);
+    stm.write(Builder::ArgType::Global);
+    stm.write<uint32_t>(0);
+    // 2
+    stm.write(Builder::OpCode::Identity);
+    stm.write(Builder::ArgType::Global);
+    stm.write<uint32_t>(1);
+    // 3
+    stm.write(Builder::OpCode::Identity);
+    stm.write(Builder::ArgType::Global);
+    stm.write<uint32_t>(2);
+    // 4
+    stm.write(Builder::OpCode::Identity);
+    stm.write(Builder::ArgType::Global);
+    stm.write<uint32_t>(3);
+    // 5
+    stm.write(Builder::OpCode::Identity);
+    stm.write(Builder::ArgType::Global);
+    stm.write<uint32_t>(4);
+    // 6
+    stm.write(Builder::OpCode::Identity);
+    stm.write(Builder::ArgType::ConstFloat64);
+    stm.write<double>(12000000000); // 12 ms if used as time
+    // 7
+    stm.write(Builder::OpCode::Identity);
+    stm.write(Builder::ArgType::ConstFloat64);
+    stm.write<double>(5000000000);
+    // 8
+    stm.write(Builder::OpCode::Identity);
+    stm.write(Builder::ArgType::ConstFloat64);
+    stm.write<double>(15000000000);
+    // [nchns: 4B][[chnname: non-empty NUL-terminated string] x nchns]
+    stm.write<uint32_t>(4);
+    stm.write_string("AWG1/OUT0/CHN1/FREQ");
+    stm.write_string("AWG1/OUT0/CHN1/AMP");
+    stm.write_string("AWG1/OUT1/CHN1/FREQ");
+    stm.write_string("AWG1/OUT1/CHN1/AMP");
+    // [ndefvals: 4B][[chnid: 4B][Type: 1B][value: 1-8B] x ndefvals]
+    stm.write<uint32_t>(0);
+    // [nslots: 4B][[Type: 1B] x nslots]
+    stm.write<uint32_t>(5);
+    stm.write(IR::Type::Float64);
+    stm.write(IR::Type::Float64);
+    stm.write(IR::Type::Float64);
+    stm.write(IR::Type::Float64);
+    stm.write(IR::Type::Bool);
+    // [nnoramp: 4B][[chnid: 4B] x nnoramp]
+    stm.write<uint32_t>(0);
+    // [nbasicseqs: 4B][[Basic Sequence] x nbasicseqs]
+    stm.write<uint32_t>(1);
+    {
+        //Basic Seq 1
+        // [ntimes: 4B][[sign: 1B][id: 4B][delta_node: 4B][prev_id: 4B] x ntimes]
+        stm.write<uint32_t>(4);
+        // t1 12 ms after start
+        stm.write<uint8_t>((uint8_t)Sign::Pos);
+        stm.write<uint32_t>(105);
+        stm.write<uint32_t>(6);
+        stm.write<uint32_t>(0);
+        // t2 5 ms after t1
+        stm.write<uint8_t>((uint8_t)Sign::Pos);
+        stm.write<uint32_t>(204);
+        stm.write<uint32_t>(7);
+        stm.write<uint32_t>(1);
+        // t3 15 ms after t2
+        stm.write<uint8_t>((uint8_t)Sign::Unknown);
+        stm.write<uint32_t>(209);
+        stm.write<uint32_t>(8);
+        stm.write<uint32_t>(2);
+        // t4 15 ms after t1
+        stm.write<uint8_t>((uint8_t)Sign::Pos);
+        stm.write<uint32_t>(209);
+        stm.write<uint32_t>(8);
+        stm.write<uint32_t>(1);
+        // [nendtimes: 4B][[time_id: 4B] x nendtimes]
+        stm.write<uint32_t>(2);
+        stm.write<uint32_t>(3);
+        stm.write<uint32_t>(4);
+        // [ntimeorders: 4B][[sign: 1B][id: 4B][before_id: 4B][after_id: 4B] x ntimeorders]
+        stm.write<uint32_t>(0);
+        // [noutputs: 4B][[id: 4B][time_id: 4B][len: 4B][val: 4B][cond: 4B][chn: 4B] x noutputs]
+        stm.write<uint32_t>(4);
+        // 1 12 ms after start, set chn1 freq to global 0
+        stm.write<uint32_t>(8); // id
+        stm.write<uint32_t>(1); // time_id
+        stm.write<uint32_t>(0); // len
+        stm.write<uint32_t>(1); // val
+        stm.write<uint32_t>(0); // cond
+        stm.write<uint32_t>(1); // chn
+        // 2 12 ms after start set chn1 amplitude to global 1
+        stm.write<uint32_t>(65); // id
+        stm.write<uint32_t>(1); // time_id
+        stm.write<uint32_t>(0); // len
+        stm.write<uint32_t>(2); // val
+        stm.write<uint32_t>(0); // cond
+        stm.write<uint32_t>(2); // chn
+        // 3 conditionally on global 4, 17 ms after start, set chn1 of out2 freq to global 2
+        stm.write<uint32_t>(22); // id
+        stm.write<uint32_t>(2); // time_id
+        stm.write<uint32_t>(0); // len
+        stm.write<uint32_t>(3); // val
+        stm.write<uint32_t>(5); // cond
+        stm.write<uint32_t>(3); // chn
+        // 4 conditionally on global 4 17 ms after start, set chn1 of out2 amp to global 3
+        stm.write<uint32_t>(75); // id
+        stm.write<uint32_t>(2); // time_id
+        stm.write<uint32_t>(0); // len
+        stm.write<uint32_t>(4); // val
+        stm.write<uint32_t>(5); // cond
+        stm.write<uint32_t>(4); // chn
+        // [nmeasures: 4B][[id: 4B][time_id: 4B][chn: 4B] x nmeasures]
+        stm.write<uint32_t>(0);
+        // [nassigns: 4B][[assign_id: 4B][global_id: 4B][val: 4B] x nassigns]
+        stm.write<uint32_t>(0);
+        // [nbranches: 4B][[branch_id: 4B][target_id: 4B][cond: 4B] x nbranches]
+        stm.write<uint32_t>(0);
+        // [default_target: 4B]
+        stm.write<uint32_t>(0);
+    }
+    // [ndatas: 4B][[ndouble: 4B][data: 8B x ndouble] x ndatas]
+    stm.write<uint32_t>(0);
+    // [nbackenddatas: 4B][[device_name: NUL-terminated string]
+    //                     [size: 4B][data: size B] x nbackenddatas]
+    stm.write<uint32_t>(0);
+
+    auto expseq = stm.create_sequence();
+
+    printf(expseq->host_seq.first_bseq ? "first_bseq" : "not_first_bseq");
+    expseq->init_run();
+    expseq->host_seq.set_global(0, 4e3);
+    expseq->host_seq.set_global(1, 0.4);
+    expseq->host_seq.set_global(2, 5e3);
+    expseq->host_seq.set_global(3, 0.1);
+    expseq->host_seq.set_global(4, 1); // conditionally turn on chn 2 pulse
+    expseq->pre_run();
+    expseq->start();
+    expseq->wait(60000);
+    expseq->post_run();
+    printf("Press any key to continue");
+    int flag = std::cin.get();
+
+    //printf("Is first bseq: %d\n", expseq->host_seq.first_bseq);
+    printf(expseq->host_seq.first_bseq ? "first_bseq" : "not_first_bseq");
+    expseq->host_seq.set_global(0, 3e3);
+    expseq->host_seq.set_global(4, 0);
+    expseq->init_run();
+    expseq->pre_run();
+    expseq->start();
+    expseq->wait(60000);
+    expseq->post_run();
 }
 
 
-
 int main() {
-    test_ramp_pulse();
+    //test_ramp_pulse();
+    //test_set_pulse_global();
+    test_set_pulse_global_mult();
 }
