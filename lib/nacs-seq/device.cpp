fea1cc30f7175802d3bfa5bb136d6456fd83d59c
kwang428 <kenneth.wang15@gmail.com>
2025-04-02 18:04:57 -0400

    [device/manager] rename finish_run to post_run and have it run after every basic sequence

--- lib/nacs-seq/device.cpp
+++ lib/nacs-seq/device.cpp
@@ -83,7 +83,7 @@ void Device::prepare_run(HostSeq&)
 {
 }
 
-void Device::finish_run(HostSeq&)
+void Device::post_run(HostSeq&)
 {
 }
 
@@ -111,7 +111,7 @@ private:
     void start(HostSeq &host_seq) override {}
     void cancel(HostSeq &host_seq) override {}
     void wait(HostSeq &host_seq) override {}
-    // void finish_run(HostSeq &host_seq) override;
+    // void post_run(HostSeq &host_seq) override;
 
     void config(const YAML::Node&) override {}
     // void parse_data(const uint8_t *data, size_t len) override;
